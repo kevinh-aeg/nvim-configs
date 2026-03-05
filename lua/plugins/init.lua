6c3f7ccfacacc7cb2ecee3e2874822f5505049b9
kevinh-aeg <khernandez@aegpresents.com>
2026-03-05 12:22:54 -0800

    update the things

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,6 +4,43 @@ return {
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
+ 
+  -- TroubleToggle [mode]  where [mode] is quickfix|lsp_workspace_diagnostics|lsp_document_diagnostics|lsp_references|loclist
+  {
+    "folke/trouble.nvim",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+
+  -- ToggleBlame [mode]  where [mode] is window|virtual
+  {
+    "FabijanZulj/blame.nvim",
+    lazy = false,
+    config = function()
+      require("blame").setup({
+        merge_consecutive = true,
+        -- virtual_style = 'float',
+      })
+    end,
+  },
+
+  {
+    "github/copilot.vim",
+    lazy = false,
+  },
+
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    lazy = false,
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      -- See Configuration section for options
+    },
+  },
 
   -- These are some examples, uncomment them if you want to see them work!
   {
@@ -14,7 +51,7 @@ return {
   },
 
   -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
+  { import = "nvchad.blink.lazyspec" },
 
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
